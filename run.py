c8ba98f81a5da12d7659a835fe949cdbb0a3a5e5
ajayanathch <ajayanath.chittela@ivtree.com>
2019-07-30 18:07:26 +0530

    for jenkins

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
-from flask import flask
+from flask import Flask
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "<h1>Hello World</h1>"
+    return "<h1>Hello World</h1>"
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0' ,port=80)
